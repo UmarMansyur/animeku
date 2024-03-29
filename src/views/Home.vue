fcdcb692d9c69278e95ae4ef75bdaa5f43b6f07d
Umar Mansyur <umar.ovie@gmail.com>
2024-03-29 21:12:44 +0700

    push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@ import { showLoader, hideLoader } from '../helpers/loader';
 const datas = ref<any>({ ongoing_anime: [], complete_anime: [] });
 
 async function loadAnimeHome() {
-  const response = await fetch('http://localhost:3000/v1/home');
+  const response = await fetch(import.meta.env.VITE_BASE_URL + '/home');
   const data = await response.json();
   datas.value = data.data;
 }
